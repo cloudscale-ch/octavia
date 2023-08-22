3837d37098bec409c80f520b4ef1337a5b492b05
Gaudenz Steinlin <gaudenz.steinlin@cloudscale.ch>
2023-08-22 09:59:00 +0200

    Raise NotFound on update of inexistent resources
    Some DB repository classes already raised NotFound on update of an
    inexistent resources. Others just returned, but did not update anything.
    This commit unifies the behavior by raising NotFound in all update
    methods if no resource is found.
    
    The missing check caused issues when two requests concurrently deleted
    the same object. We encountered this for HealthMonitor resources, but
    other resources are probably affected by the same issue.
    
    1. The first request is received and the LB locked (=PENDING_UPDATE)
       and the HealthMonitor set to PENDING_DELETE. A 204 NO CONTENT
       response is sent to the client.
    
    2. The second request is received while the health monitor still
       exists. Thus no immediate 404 NOT FOUND response is sent.
    
    3. The health monitor deletion job finishes and the LB is unlocked
       (=ACTIVE).
    
    4. The second request proceeds and is able to lock the LB
       (=PENDING_UPDATE). The health monitor repository update also
       succeeds, but does not update any object. A 204 NO CONTENT
       response is sent to the client.
    
    5. The deletion job issued by the second request fails because the
       health monitor no longer exists in the database. Because this
       happens during setup of the deletion taskflow in the worker, no
       flow is started and the LB is stuck in the PENDING_UPDATE state.
    
    With this change, the second health monitor DELETE request fails in step
    4 and returns NotFound.
    
    Change-Id: I4f858a71784ae640bb6b5eddf9347c7b70afa632
    (cherry picked from commit e96c613829b413381e82d5b7faaad531ff5c980a)

--- octavia/db/repositories.py
+++ octavia/db/repositories.py
@@ -109,11 +109,15 @@ class BaseRepository(object):
             if tags is not None:
                 resource = session.query(self.model_class).get(id)
                 resource.tags = tags
-            (session.query(self.model_class)
+            count = (session.query(self.model_class)
              .filter_by(id=id)
              .populate_existing()
              .with_for_update()
              .update(model_kwargs))
+
+            if count == 0:
+                raise exceptions.NotFound(
+                    resource=self.model_class._name(), id=id)
 
     def get(self, session, **filters):
         """Retrieves an entity from the database.
@@ -950,11 +954,16 @@ class VipRepository(BaseRepository):
     def update(self, session, load_balancer_id, **model_kwargs):
         """Updates a vip entity in the database by load_balancer_id."""
         with session.begin(subtransactions=True):
-            (session.query(self.model_class)
+            count = (session.query(self.model_class)
              .filter_by(load_balancer_id=load_balancer_id)
              .populate_existing()
              .with_for_update()
              .update(model_kwargs))
+
+            if count == 0:
+                raise exceptions.NotFound(
+                    resource=f'{self.model_class._name()} on LoadBalancer',
+                    id=load_balancer_id)
 
 
 class AdditionalVipRepository(BaseRepository):
@@ -1032,11 +1041,17 @@ class SessionPersistenceRepository(BaseRepository):
     def update(self, session, pool_id, **model_kwargs):
         """Updates a session persistence entity in the database by pool_id."""
         with session.begin(subtransactions=True):
-            (session.query(self.model_class)
+            count = (session.query(self.model_class)
              .filter_by(pool_id=pool_id)
              .populate_existing()
              .with_for_update()
              .update(model_kwargs))
+
+            if count == 0:
+                raise exceptions.NotFound(
+                    resource=f'{self.model_class._name()} on Pool',
+                    id=pool_id
+                )
 
     def exists(self, session, pool_id):
         """Checks if session persistence exists on a pool."""
@@ -1367,11 +1382,17 @@ class ListenerStatisticsRepository(BaseRepository):
 
         """
         with session.begin(subtransactions=True):
-            (session.query(self.model_class)
+            count = (session.query(self.model_class)
              .filter_by(listener_id=listener_id)
              .populate_existing()
              .with_for_update()
              .update(model_kwargs))
+
+            if count == 0:
+                raise exceptions.NotFound(
+                    resource=f'{self.model_class._name()} on Listener',
+                    id=listener_id,
+                )
 
 
 class AmphoraRepository(BaseRepository):
@@ -1707,17 +1728,24 @@ class SNIRepository(BaseRepository):
             raise exceptions.MissingArguments
         with session.begin(subtransactions=True):
             if listener_id:
-                (session.query(self.model_class)
+                count = (session.query(self.model_class)
                  .filter_by(listener_id=listener_id)
                  .populate_existing()
                  .with_for_update()
                  .update(model_kwargs))
             elif tls_container_id:
-                (session.query(self.model_class)
+                count = (session.query(self.model_class)
                  .filter_by(tls_container_id=tls_container_id)
                  .populate_existing()
                  .with_for_update()
                  .update(model_kwargs))
+
+            if count == 0:
+                raise exceptions.NotFound(
+                    resource=f'{self.model_class._name()} on '
+                             f'{listener_id and "Listener" or "TLSContainer"}',
+                    id=listener_id or tls_container_id,
+                )
 
 
 class AmphoraHealthRepository(BaseRepository):
@@ -1726,11 +1754,17 @@ class AmphoraHealthRepository(BaseRepository):
     def update(self, session, amphora_id, **model_kwargs):
         """Updates a healthmanager entity in the database by amphora_id."""
         with session.begin(subtransactions=True):
-            (session.query(self.model_class)
+            count = (session.query(self.model_class)
              .filter_by(amphora_id=amphora_id)
              .populate_existing()
              .with_for_update()
              .update(model_kwargs))
+
+            if count == 0:
+                raise exceptions.NotFound(
+                    resource=f'{self.model_class._name()} on Amphora',
+                    id=amphora_id,
+                )
 
     def replace(self, session, amphora_id, **model_kwargs):
         """replace or insert amphora into database."""
@@ -1820,11 +1854,17 @@ class VRRPGroupRepository(BaseRepository):
     def update(self, session, load_balancer_id, **model_kwargs):
         """Updates a VRRPGroup entry for by load_balancer_id."""
         with session.begin(subtransactions=True):
-            (session.query(self.model_class)
+            count = (session.query(self.model_class)
              .filter_by(load_balancer_id=load_balancer_id)
              .populate_existing()
              .with_for_update()
              .update(model_kwargs))
+
+            if count == 0:
+                raise exceptions.NotFound(
+                    resource=f'{self.model_class._name()} on LoadBalancer',
+                    id=load_balancer_id,
+                )
 
 
 class L7RuleRepository(BaseRepository):
@@ -2254,11 +2294,15 @@ class AvailabilityZoneRepository(_GetALLExceptDELETEDIdMixin, BaseRepository):
         :returns: octavia.common.data_model
         """
         with session.begin(subtransactions=True):
-            (session.query(self.model_class)
+            count = (session.query(self.model_class)
              .filter_by(name=name)
              .populate_existing()
              .with_for_update()
              .update(model_kwargs))
+
+            if count == 0:
+                raise exceptions.NotFound(resource=self.mode_class._name(),
+                                          id=name)
 
     def delete(self, serial_session, **filters):
         """Special delete method for availability_zone.
